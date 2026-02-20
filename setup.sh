9de12726a80bed8f75be4dda293b95aec98fc1a5
sakibtalukdar280-debug <sakibtalukdar280@gmail.com>
2026-02-21 04:57:55 +0600

    Initialize setup script for environment configuration
    Set up environment by updating packages, installing Python and Git, and cloning the Update-bomber repository.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+pkg update && pkg upgrade -y
+pkg install python -y
+pkg install git -y
+pip install requests
+pip install bs4 requests
+rm -rf Update-bomber 
+git clone https://github.com/DH-Alamin/Update-bomber
+ls
+cd Update-bomber
+ls
+python V8.py
